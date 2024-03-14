e92520015e53379f4c18b783b1caa13388fd714d
hwrdyen <hwrdpvt@gmail.com>
2024-03-13 21:43:57 -0400

    Updated footer components

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,14 +1,29 @@
 import "./Footer.scss";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Footer = () => {
   return (
-    <div>
-      <div>--Footer Component--</div>
-      <span>Readable</span>
-      <span>
-        <p>Privacy Policy</p>
-        <p>Terms of Service</p>
-      </span>
+    <div className="Footer__Container">
+      <div className="Footer__Info--container">
+        <div>Contact</div>
+        <div>About Us</div>
+        <div>Quick Links</div>
+        <div>Recent Posts</div>
+      </div>
+      <div className="Footer__Legal--container">
+        <div>© BucketBlast Corporation</div>
+        <div className="Footer__SocialMedia--container">
+          <LinkedInIcon />
+          <InstagramIcon />
+          <FacebookIcon />
+          <XIcon />
+          <YouTubeIcon />
+        </div>
+      </div>
     </div>
   );
 };
